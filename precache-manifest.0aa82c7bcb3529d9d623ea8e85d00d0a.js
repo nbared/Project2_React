19254315b0a1c0c9704c2d9a5ac415442e580ae9
nicobared <nicobared@gmail.com>
2019-11-20 14:41:50 -0500

    Updates

---
+++ precache-manifest.0aa82c7bcb3529d9d623ea8e85d00d0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "091216a5a2aa58ff8791f11f02fceb32",
+    "url": "/Project2_React/index.html"
+  },
+  {
+    "revision": "b19b1fb8c7982d6216e1",
+    "url": "/Project2_React/static/css/main.e5aa6b5d.chunk.css"
+  },
+  {
+    "revision": "b81243c424e0dec58237",
+    "url": "/Project2_React/static/js/2.d3d40ab7.chunk.js"
+  },
+  {
+    "revision": "b19b1fb8c7982d6216e1",
+    "url": "/Project2_React/static/js/main.dda177d4.chunk.js"
+  },
+  {
+    "revision": "a46b69e3a0448e7a668e",
+    "url": "/Project2_React/static/js/runtime-main.de4cd053.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/Project2_React/static/media/logo.25bf045c.svg"
+  }
+]);
